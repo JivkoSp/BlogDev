b9bda6f8cdbccad4b5bdc1e8c57475b432331299
JivkoSp <atjspasov@gmail.com>
2024-11-17 19:44:44 +0200

    Added NEW class: Data/BlogApiJsonDirectAccessSetting.cs

---
+++ Data/BlogApiJsonDirectAccessSetting.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Data
+{
+    internal sealed class BlogApiJsonDirectAccessSetting
+    {
+        public string BlogPostsFolder { get; set; } = string.Empty;
+
+        public string CategoriesFolder { get; set; } = string.Empty;
+
+        public string TagsFolder { get; set; } = string.Empty;
+
+        public string CommentsFolder { get; set; } = string.Empty;
+
+        public string DataPath { get; set; } = string.Empty;
+    }
+}
