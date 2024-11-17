bb3a349fbee87537754367ec1ab677138670d0d9
JivkoSp <atjspasov@gmail.com>
2024-11-17 19:21:58 +0200

    Added NEW class: Data.Models/Category.cs

---
+++ Data.Models/Category.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Data.Models
+{
+    internal sealed class Category
+    {
+        public string? Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+    }
+}
